c94e81d463e77eba5bf653cf7557702bc88f67e2
AndreyMalykhin <shell100500@gmail.com>
2018-01-04 15:36:10 +0200

    Added Apollo

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,11 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
 import Hello from "components/hello";
 import Home from "components/home";
 import "intl";
 import * as React from "react";
+import { ApolloProvider } from "react-apollo";
 import { addLocaleData, IntlProvider } from "react-intl";
 import * as ru from "react-intl/locale-data/ru";
 import { StyleSheet, Text, View } from "react-native";
@@ -9,31 +13,38 @@ import { Link, NativeRouter, Route, Switch } from "react-router-native";
 
 addLocaleData(ru);
 const messages = {
-  hello: "Привет {platform}!",
+    hello: "Привет {platform}!",
 };
 
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({ uri: "https://api.graph.cool/simple/v1/swapi" }),
+});
+
 export default class App extends React.Component {
-  public render() {
-    return (
-      <IntlProvider locale="ru" textComponent={Text} messages={messages}>
-        <NativeRouter>
-          <View style={styles.container}>
-            <Switch>
-              <Route exact={true} path="/hello" component={Hello}/>
-              <Route component={Home}/>
-            </Switch>
-          </View>
-        </NativeRouter>
-      </IntlProvider>
-    );
-  }
+    public render() {
+        return (
+            <ApolloProvider client={client}>
+                <IntlProvider locale="ru" textComponent={Text} messages={messages}>
+                    <NativeRouter>
+                        <View style={styles.container}>
+                            <Switch>
+                                <Route exact={true} path="/hello" component={Hello} />
+                                <Route component={Home} />
+                            </Switch>
+                        </View>
+                    </NativeRouter>
+                </IntlProvider>
+            </ApolloProvider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center",
-  },
+    container: {
+        alignItems: "center",
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: "center",
+    },
 });
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,47 @@
+import gql from "graphql-tag";
 import * as React from "react";
+import { ChildProps, graphql, QueryProps } from "react-apollo";
 import { Text, View } from "react-native";
 import { Link } from "react-router-native";
 
-class Home extends React.Component {
+class Home extends React.Component<IProps, {}> {
     public render() {
+        const { loading, error, Starship } = this.props.data;
         return (
             <View>
+                <Text>{!loading && Starship.name}</Text>
                 <Text>Open up App.js to start working on your app!</Text>
                 <Text>Changes you make will automatically reload.</Text>
                 <Text>Shake your phone to open the developer menu.</Text>
                 <Link to="/hello">
-                    <Text style={{color: "blue"}}>Hello</Text>
+                    <Text style={{ color: "blue" }}>Hello</Text>
                 </Link>
             </View>
         );
     }
 }
 
-export default Home;
+const MY_QUERY = gql`
+query GetStarship($id: ID!) {
+    Starship(id: $id) {
+        name
+        hyperdriveRating
+    }
+}`;
+
+interface IResponse {
+    Starship: {
+        name: string;
+        hyperdriveRating: number;
+    };
+}
+
+interface IProps {
+    data: QueryProps & IResponse;
+}
+
+export default graphql<IResponse, {}, IProps>(MY_QUERY, {
+    options: {
+        variables: { id: "cj0nwtqpzq4tt01142nh7e9i4" },
+    },
+})(Home);
